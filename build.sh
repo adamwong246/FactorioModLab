c983c44a2da65293f1f6b9f3889004b85189a4a2
adam wong <adamwong246@gmail.com>
2021-01-27 09:44:47 -0800

    update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,21 @@
-VERSION=$(cat ./mods/$1/info.json | jq -r '.version')
-echo "making project: $1 v$VERSION"
+#!/bin/bash
 
+# Build a Factorio mod by name.
+
+set -e
+
+# Run luajit to check the files for easy syntax errors
+find ./mods/$1 -name "*.lua"|while read fname; do
+  luajit -bl $fname > /dev/null
+done
+
+# get the version from info.json
+VERSION=$(cat ./mods/$1/info.json | jq -r '.version')
+
+
+# make the zip file
 cd mods
-zip "$1_$VERSION.zip" ./$1/*
+zip -r "$1_$VERSION.zip" ./$1/*
 mv "$1_$VERSION.zip" ..
+
+echo "$1_$VERSION.zip ✅ "
