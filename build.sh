9c270a6161facf985a541e147c42c746b93d3d18
adam wong <adamwong246@gmail.com>
2021-01-30 09:41:42 -0800

    update

--- build.sh
+++ build.sh
@@ -7,15 +7,17 @@ set -e
 # Run luajit to check the files for easy syntax errors
 find ./mods/$1 -name "*.lua"|while read fname; do
   luajit -bl $fname > /dev/null
+  echo "$fname ✅"
 done
 
 # get the version from info.json
 VERSION=$(cat ./mods/$1/info.json | jq -r '.version')
 
+# rm "$1_$VERSION.zip"
 
 # make the zip file
 cd mods
 zip -r "$1_$VERSION.zip" ./$1/*
 mv "$1_$VERSION.zip" ..
 
-echo "$1_$VERSION.zip ✅ "
+echo "$1_$VERSION.zip ✅"
--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
+echo "copying $1 to the mods folder..."
 cp $1 ~/Library/Application\ Support/factorio/mods
