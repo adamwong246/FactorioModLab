7d5e1383d6ceabc06b64a55d99ea3551d3abd588
adam wong <adamwong246@gmail.com>
2021-02-01 15:36:45 -0800

    add science-packs-go-bad

--- build.sh
+++ build.sh
@@ -4,20 +4,45 @@
 
 set -e
 
-# Run luajit to check the files for easy syntax errors
-find ./mods/$1 -name "*.lua"|while read fname; do
-  luajit -bl $fname > /dev/null
-  echo "$fname ✅"
-done
+echo "luajit-ing..."
+while read p; do
+  # Run luajit to check the files for easy syntax errors
+  find ./mods/$1/$p -name "*.lua"|while read fname; do
+    luajit -bl $fname > /dev/null && echo "$fname ✅"
+
+  done
+
+done <./mods/$1/.fmodlab
 
 # get the version from info.json
 VERSION=$(cat ./mods/$1/info.json | jq -r '.version')
 
 # rm "$1_$VERSION.zip"
+cd mods/$1
 
-# make the zip file
-cd mods
-zip -r "$1_$VERSION.zip" ./$1/*
-mv "$1_$VERSION.zip" ..
+echo "building..."
+rm -rf "$1_$VERSION"
+mkdir "$1_$VERSION"
 
+while read p; do
+  # Run luajit to check the files for easy syntax errors
+  find $p |while read fname; do
+
+    # luajit -bl $fname > /dev/null
+
+    # cp --parents  $fname "$1_$VERSION"
+    # ditto $fname "$1_$VERSION"
+    rsync -R $fname "$1_$VERSION"
+
+    echo "$fname -> $1_$VERSION..."
+  done
+
+done <.fmodlab
+
+zip "$1_$VERSION.zip" -r "$1_$VERSION"
+rm -rf "$1_$VERSION"
 echo "$1_$VERSION.zip ✅"
+
+#
+# mv "$1_$VERSION.zip" ../..
+#
---
+++ make.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Build and install a Factorio mod by name.
+
+set -e
+
+sh build.sh chernobyl
