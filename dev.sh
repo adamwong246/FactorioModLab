af029e38c97578f2e3d55719fb3783af3c0c6b45
adam wong <adamwong246@gmail.com>
2022-08-24 20:25:04 -0700

    wtf

---
+++ dev.sh
@@ -0,0 +1,1 @@
+killall factorio & sh build.sh $1 && sh install.sh ./mods/$1/$1_$2.zip && sleep 1 && open ~/Library/Application\ Support/Steam/steamapps/common/Factorio/factorio.app
--- make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build and install a Factorio mod by name.
-
-set -e
-
-sh build.sh chernobyl
